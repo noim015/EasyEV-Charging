1cb725e5afd43e9d75804ead662d9bef75dc2077
noim015 <sayed.noim.ahmed@gmail.com>
2025-05-25 12:04:00 +1000

    Create Add Location

---
+++ public/add_location.php
@@ -0,0 +1,46 @@
+<?php
+require_once '../init.php';
+require_once '../classes/Location.php';
+
+if (!isAdmin()) {
+    echo "Unauthorized access.";
+    exit;
+}
+
+$msg = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $desc = $_POST['description'] ?? '';
+    $stations = $_POST['num_stations'] ?? 0;
+    $cost = $_POST['cost_per_hour'] ?? 0.00;
+
+    $location = new Location();
+    if ($location->addLocation($desc, (int)$stations, (float)$cost)) {
+        $msg = "Location added successfully.";
+    } else {
+        $msg = "Failed to add location.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Charging Location</title>
+</head>
+<body>
+    <h2>Add New Charging Location</h2>
+    <form method="POST">
+        <label>Description:</label><br>
+        <input type="text" name="description" required><br><br>
+
+        <label>Number of Stations:</label><br>
+        <input type="number" name="num_stations" required><br><br>
+
+        <label>Cost Per Hour ($):</label><br>
+        <input type="number" name="cost_per_hour" step="0.01" required><br><br>
+
+        <input type="submit" value="Add Location">
+    </form>
+    <p style="color:green"><?= $msg ?></p>
+</body>
+</html>
