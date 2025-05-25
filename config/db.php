d688e9d3ee58c8202f8468c6d1bfbb216a5abd2c
noim015 <sayed.noim.ahmed@gmail.com>
2025-05-25 11:41:04 +1000

    Create DB Connection

---
+++ config/db.php
@@ -0,0 +1,17 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "easy_ev_charging";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function connect() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        if ($this->conn->connect_error) {
+            die("DB Connection failed: " . $this->conn->connect_error);
+        }
+        return $this->conn;
+    }
+}
+?>
